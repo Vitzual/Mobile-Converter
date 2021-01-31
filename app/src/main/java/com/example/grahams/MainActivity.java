dce5f8419639dde03e980986018c02bfc281a840
Vitzual <65577955+Vitzual@users.noreply.github.com>
2021-01-31 12:48:43 -0800

    Added default variables and listners in main

--- app/src/main/java/com/example/grahams/MainActivity.java
+++ app/src/main/java/com/example/grahams/MainActivity.java
@@ -3,12 +3,45 @@ package com.example.grahams;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Set variables for radio group and radio buttons
+    RadioGroup radioGroup;
+    RadioButton radioButton;
+
+    // Set variable for the edit text field
+    EditText numberToCalculate;
+
+    // On create, create the on click listener for the calculate button
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        numberToCalculate = findViewById(R.id.numberInput);
+        radioGroup = findViewById(R.id.radioGroup);
+        Button buttonCalculate = findViewById(R.id.button_apply);
+        buttonCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    // Get the selected radio button
+    public void checkButton(View v) {
+        int radioId = radioGroup.getCheckedRadioButtonId();
+
+        radioButton = findViewById(radioId);
+
+        //Toast.makeText(context: this, "Selected")
     }
 }
